f2e8420c39d3455c3ff5d347d5602107fefd430b
NoWizards <nowizards@hotmail.es>
2017-12-04 11:30:03 -0500

    RxJs Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-    .then(dish=> this.dish = dish );
+    .subscribe(dish=> this.dish = dish );
     this.promotion = this.promotionservice.getFeaturedPromotion();
   }
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,11 +5,13 @@ import {Dish} from '../shared/dish';
 import { DISHES} from '../shared/dishes';
 import { DishService } from '../services/dish.service';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
+
 export class MenuComponent implements OnInit {
 
   dishes: Dish[];
@@ -20,8 +22,7 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
     this.dishService.getDishes()
-    .then(dishes => this.dishes = dishes)
-    .catch(msg => console.log("promise failed"));
+    .subscribe(dishes => this.dishes = dishes);
   }
   onSelect(dish:Dish){
     this.selectedDish = dish;
